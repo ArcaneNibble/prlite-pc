50d48adfb38f424f043e0ee20231a95dc2d2810a
alan <downingbots@gmail.com>
2015-02-24 19:39:23 -0800

    chess changes

---
+++ catkin/pr2lite_chess_msgs/cmake/pr2lite_chess_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(pr2lite_chess_msgs_MESSAGE_FILES "/home/ros/catkin_ws/src/pr2lite_chessbox/pr2lite_chess_msgs/msg/ChessPiece.msg;/home/ros/catkin_ws/src/pr2lite_chessbox/pr2lite_chess_msgs/msg/ChessBoard.msg")
+set(pr2lite_chess_msgs_SERVICE_FILES "")
---
+++ catkin/pr2lite_chess_msgs/cmake/pr2lite_chess_msgs-msg-paths.cmake
@@ -0,0 +1,5 @@
+# generated from genmsg/cmake/pkg-msg-paths.cmake.em
+
+# message include dirs in develspace
+set(pr2lite_chess_msgs_MSG_INCLUDE_DIRS "/home/ros/catkin_ws/src/pr2lite_chessbox/pr2lite_chess_msgs/msg")
+set(pr2lite_chess_msgs_MSG_DEPENDENCIES geometry_msgs)
